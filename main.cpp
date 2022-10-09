fd7f6553051ba1b193432872fa007deeb4226178
Marvish Subhas Chandra <mschandr@uci.edu>
2022-10-09 09:08:32 -0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,182 @@
+// Marvish Chandra
+
+class MarvelAvengersCostumes{
+    public:
+    string costume;
+    string costumeSize;
+};
+int main(){
+    MarvelAvengersCostumes MarvelAvengersCostumesObj1;
+    MarvelAvengersCostumesObj1.costume = "Adult Captain America Costume - Marvel The Falcon and the Winter Soldier";
+    MarvelAvengersCostumesObj1.costumesSize = "Standard Size";
+
+    MarvelAvengersCostumes MarvelAvengersCostumesObj2;
+    MarvelAvengersCostumesObj2.costume = "Adult Black Widow Costume - Black Widow Movie";
+    MarvelAvengersCostumesObj2.costumesSize = "Medium";
+
+    MarvelAvengersCostumes MarvelAvengersCostumesObj3;
+    MarvelAvengersCostumesObj3.costume = "Child Iron Man Costume - Marvel's Avengers Game";
+    MarvelAvengersCostumesObj3.costumesSize = "Small","Medium","Large";
+
+    MarvelAvengersCostumes MarvelAvengersCostumesObj4;
+    MarvelAvengersCostumesObj4.costume = "Child Black Widow Costume - Black Widow Movie";
+    MarvelAvengersCostumesObj4.costumesSize = "Small","Medium";
+
+    MarvelAvengersCostumes MarvelAvengersCostumesObj5;
+    MarvelAvengersCostumesObj5.costume = "Baby Hulk Muscle Costume";
+    MarvelAvengersCostumesObj5.costumesSize = "Zero to Six Months","Six to Twelve Months","Twelve to Twenty-four Months";
+
+    MarvelAvengersCostumes MarvelAvengersCostumesObj6;
+    MarvelAvengersCostumesObj6.costume = "Toddler Boys Captain America Muscle Costume Classic";
+    MarvelAvengersCostumesObj6.costumesSize = "Two Toddler","Three to Four Toddler";
+
+    MarvelAvengersCostumes MarvelAvengersCostumesObj7;
+    MarvelAvengersCostumesObj7.costume = "Black Panther Dog Costume";
+    MarvelAvengersCostumesObj7.costumesSize = "Small";
+
+    MarvelAvengersCostumes MarvelAvengersCostumesObj8;
+    MarvelAvengersCostumesObj8.costume = "Adult Hulk Plus Size Muscle Costume - Marvel";
+    MarvelAvengersCostumesObj8.costumesSize = "Plus";
+
+    MarvelAvengersCostumes MarvelAvengersCostumesObj9;
+    MarvelAvengersCostumesObj9.costume = "Adult Hulk Muscle Costume - Marvel";
+    MarvelAvengersCostumesObj9.costumesSize = "Standard";
+
+    MarvelAvengersCostumes MarvelAvengersCostumesObj10;
+    MarvelAvengersCostumesObj10.costume = "Adult Mighty Thor Costume - Thor: Love and Thunder";
+    MarvelAvengersCostumesObj10.costumesSize = "Small","Medium","Large","Extra Large";
+
+    MarvelAvengersCostumes MarvelAvengersCostumesObj11;
+    MarvelAvengersCostumesObj11.costume = "Adult Doctor Strange Plus Size Costume - Marvel Doctor Strange in the Multiverse of Madness";
+    MarvelAvengersCostumesObj11.costumesSize = "Plus";
+
+    MarvelAvengersCostumes MarvelAvengersCostumesObj12;
+    MarvelAvengersCostumesObj12.costume = "Adult Spider-Girl Costume - Marvel";
+    MarvelAvengersCostumesObj12.costumesSize = "Small","Medium","Large","Extra Large";
+
+    MarvelAvengersCostumes MarvelAvengersCostumesObj13;
+    MarvelAvengersCostumesObj13.costume = "Adult Marvel Taskmaster Costume - Black Widow";
+    MarvelAvengersCostumesObj13.costumesSize = "Standard Size";
+
+    MarvelAvengersCostumes MarvelAvengersCostumesObj14;
+    MarvelAvengersCostumesObj14.costume = "Adult Thor Costume - Thor: Love and Thunder";
+    MarvelAvengersCostumesObj14.costumesSize = "Standard Size";
+
+    MarvelAvengersCostumes MarvelAvengersCostumesObj15;
+    MarvelAvengersCostumesObj15.costume = "Adult Doctor Strange Costume - Marvel Doctor Strange in the Multiverse of Madness";
+    MarvelAvengersCostumesObj15.costumesSize = "Standard Size";
+
+    MarvelAvengersCostumes MarvelAvengersCostumesObj16;
+    MarvelAvengersCostumesObj16.costume = "Adult Scarlet Witch Costume - Marvel Doctor Strange in the Multiverse of Madness";
+    MarvelAvengersCostumesObj16.costumesSize = "Small","Medium","Large","Extra Large";
+
+    MarvelAvengersCostumes MarvelAvengersCostumesObj17;
+    MarvelAvengersCostumesObj17.costume = "Adult Iron Man Costume - Marvel's Avengers Game";
+    MarvelAvengersCostumesObj17.costumesSize = "Standard";
+
+    MarvelAvengersCostumes MarvelAvengersCostumesObj18;
+    MarvelAvengersCostumesObj18.costume = "Adult Marvel Task Master Costume Plus Size - Black Widow";
+    MarvelAvengersCostumesObj18.costumesSize = "Plus";
+
+    MarvelAvengersCostumes MarvelAvengersCostumesObj19;
+    MarvelAvengersCostumesObj19.costume = "Adult Black Panther Muscle Costume - Black Panther";
+    MarvelAvengersCostumesObj19.costumesSize = "Standard";
+
+    MarvelAvengersCostumes MarvelAvengersCostumesObj20;
+    MarvelAvengersCostumesObj20.costume = "Adult Spider-Girl Plus Size Costume - Marvel";
+    MarvelAvengersCostumesObj20.costumesSize = "Plus";
+
+    MarvelAvengersCostumes MarvelAvengersCostumesObj21;
+    MarvelAvengersCostumesObj21.costume = "Adult Spider-Girl Plus Size Costume - Marvel";
+    MarvelAvengersCostumesObj21.costumesSize = "Plus";
+
+    MarvelAvengersCostumes MarvelAvengersCostumesObj22;
+    MarvelAvengersCostumesObj22.costume = "Adult Captain America Muscle Costume Plus Size";
+    MarvelAvengersCostumesObj22.costumesSize = "Plus";
+
+    MarvelAvengersCostumes MarvelAvengersCostumesObj23;
+    MarvelAvengersCostumesObj23.costume = "Adult Thor Plus Size Costume - Thor: Love and Thunder";
+    MarvelAvengersCostumesObj23.costumesSize = "Plus";
+
+    MarvelAvengersCostumes MarvelAvengersCostumesObj24;
+    MarvelAvengersCostumesObj24.costume = "Kids' Black Panther Costume - Legacy";
+    MarvelAvengersCostumesObj24.costumesSize = "Small","Medium","Large";
+
+    MarvelAvengersCostumes MarvelAvengersCostumesObj25;
+    MarvelAvengersCostumesObj25.costume = "Boys Captain America Muscle Costume Classic";
+    MarvelAvengersCostumesObj25.costumesSize = "Large";
+
+    MarvelAvengersCostumes MarvelAvengersCostumesObj26;
+    MarvelAvengersCostumesObj26.costume = "Kids' Captain America Costume - Marvel The Falcon and the Winter Soldier";
+    MarvelAvengersCostumesObj26.costumesSize = "Small","Medium";
+
+    MarvelAvengersCostumes MarvelAvengersCostumesObj27;
+    MarvelAvengersCostumesObj27.costume = "Kids' Iron Man Costume - Marvel";
+    MarvelAvengersCostumesObj27.costumesSize = "Three to Four Toddler","Small","Medium","Large";
+
+    MarvelAvengersCostumes MarvelAvengersCostumesObj28;
+    MarvelAvengersCostumesObj28.costume = "Boys Captain America Costume Classic";
+    MarvelAvengersCostumesObj28.costumesSize = "Small","Medium","Large";
+
+    MarvelAvengersCostumes MarvelAvengersCostumesObj29;
+    MarvelAvengersCostumesObj29.costume = "Kids' Captain America Muscle Costume - Marvel";
+    MarvelAvengersCostumesObj29.costumesSize = "Three to Four Toddler","Small","Medium","Large";
+
+    MarvelAvengersCostumes MarvelAvengersCostumesObj30;
+    MarvelAvengersCostumesObj30.costume = "Kids' Pink & Blue Spider-Girl Costume - Marvel";
+    MarvelAvengersCostumesObj30.costumesSize = "Three to Four Toddler","Small","Medium","Large";
+
+    MarvelAvengersCostumes MarvelAvengersCostumesObj31;
+    MarvelAvengersCostumesObj31.costume = "Kids' Thor Costume - Thor: Love and Thunder";
+    MarvelAvengersCostumesObj31.costumesSize = "Small","Medium","Large";
+
+    MarvelAvengersCostumes MarvelAvengersCostumesObj32;
+    MarvelAvengersCostumesObj32.costume = "Kids' Peter Parker Spider-Man Costume - Marvel Spidey & His Amazing Friends";
+    MarvelAvengersCostumesObj32.costumesSize = "Two Toddler","Three to Four Toddler";
+
+    MarvelAvengersCostumes MarvelAvengersCostumesObj33;
+    MarvelAvengersCostumesObj33.costume = "Kids' Miles Morales Spider-Man Costume - Marvel Spidey & His Amazing Friends";
+    MarvelAvengersCostumesObj33.costumesSize = "Three to Four Toddler";
+
+    MarvelAvengersCostumes MarvelAvengersCostumesObj34;
+    MarvelAvengersCostumesObj34.costume = "Captain America Shield";
+    MarvelAvengersCostumesObj34.costumesSize = "Standard Size";
+
+    MarvelAvengersCostumes MarvelAvengersCostumesObj35;
+    MarvelAvengersCostumesObj36.costume = "Thor Hammer";
+    MarvelAvengersCostumesObj36.costumesSize = "Standard Size";
+
+    MarvelAvengersCostumes MarvelAvengersCostumesObj36;
+    MarvelAvengersCostumesObj36.costume = "Child Plastic Captain America Mask";
+    MarvelAvengersCostumesObj36.costumesSize = "Standard Size";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
